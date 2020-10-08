f716be491a7f71ddbc5cc90a1ddcba84735b83bc
David Ortega <david.ortega@habla.io>
2020-10-08 00:52:43 -0300

    0001 instalar ant-d e importat el carousel

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <div className="elementos">Extendimos hasta el 4 de Octubre</div>
+      <div className="elementos">Cyberfashion</div>
+      <div className="elementos">Hasta el 40% de descuento</div>
+    </div>
+  );
+}
+
+export default NavBar;
