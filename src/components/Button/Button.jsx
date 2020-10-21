ce32eb5180d84a1543ec01ff669db7e3f17d94a7
Camilo Mesa Suarez <camilo@MacBook-Pro-de-Camilo.local>
2020-10-21 00:22:59 -0300

    avance en los botones, que quilombo

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,18 +2,31 @@ import React from "react";
 import { Button as Boton, Tooltip } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
 
-class Button extends React.Component {
-  render() {
-    return (
-      <>
-        <Tooltip title="search">
-          <Boton type="primary" shape="rectangle">
-            <p>buscar</p>
-          </Boton>
-        </Tooltip>
-      </>
-    );
-  }
-}
+// class Button extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <Tooltip title="search">
+//           <Boton type="primary" shape="rectangle">
+//             {this.props.title}
+//           </Boton>
+//         </Tooltip>
+//       </>
+//     );
+//   }
+// }
+
+const Button = function ({ title }) {
+  console.log(`teta`);
+
+  return (<>
+    <Tooltip title="search">
+      <Boton type="primary" shape="rectangle">
+        {title}
+      </Boton>
+    </Tooltip>
+  </>)
+};
+
 
 export default Button;
